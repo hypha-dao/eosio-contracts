5ebf8647260127b2298ed9f71fe7564efea9fe9f
Max <max@digitalscarcity.io>
2020-11-14 11:44:26 -0500

    integrated functional go tests into contracts repo

---
+++ dao-go/badges_test.go
@@ -0,0 +1,295 @@
+package dao_test
+
+const enroller_badge = `{
+    "content_groups": [
+        [
+            {
+                "label": "content_group_label",
+                "value": [
+                    "string",
+                    "details"
+                ]
+            },
+            {
+                "label": "title",
+                "value": [
+                    "string",
+                    "Enroller"
+                ]
+            },
+            {
+                "label": "description",
+                "value": [
+                    "string",
+                    "Enrollers authorize new membership and hold administative duties of the Hypha organization"
+                ]
+            },
+            {
+                "label": "icon",
+                "value": [
+                    "string",
+                    "https://gallery.yopriceville.com/var/resizes/Free-Clipart-Pictures/Badges-and-Labels-PNG/Gold_Badge_Transparent_PNG_Image.png"
+                ]
+            },
+            {
+                "label": "seeds_coefficient_x10000",
+                "value": [
+                    "int64",
+                    10100
+                ]
+			},
+			{
+                "label": "hypha_coefficient_x10000",
+                "value": [
+                    "int64",
+                    10100
+                ]
+			},
+			{
+                "label": "husd_coefficient_x10000",
+                "value": [
+                    "int64",
+                    10100
+                ]
+			},
+			{
+                "label": "hvoice_coefficient_x10000",
+                "value": [
+                    "int64",
+                    10100
+                ]
+            }
+        ]
+    ]
+}`
+
+const enroller_badge_assignment = `{
+    "content_groups": [
+        [
+            {
+                "label": "content_group_label",
+                "value": [
+                    "string",
+                    "details"
+                ]
+            },
+            {
+                "label": "title",
+                "value": [
+                    "string",
+                    "Enroller Badge Assignment"
+                ]
+            },
+            {
+                "label": "description",
+                "value": [
+                    "string",
+                    "Enrollers authorize new membership and hold administative duties of the Hypha organization"
+                ]
+            },
+            {
+                "label": "start_period",
+                "value": [
+                    "int64",
+                    2
+                ]
+            },
+            {
+                "label": "end_period",
+                "value": [
+                    "int64",
+                    9
+                ]
+            }
+        ]
+    ]
+}`
+
+const librarian_badge_proposal = `{
+    "content_groups": [
+        [
+            {
+                "label": "content_group_label",
+                "value": [
+                    "string",
+                    "details"
+                ]
+            },
+            {
+                "label": "title",
+                "value": [
+                    "string",
+                    "Badge Librarian"
+                ]
+            },
+            {
+                "label": "description",
+                "value": [
+                    "string",
+                    "Ensure that badges represent clear, clean, and useful distinctions for the organization"
+                ]
+            },
+            {
+                "label": "icon",
+                "value": [
+                    "string",
+                    "https://gallery.yopriceville.com/var/resizes/Free-Clipart-Pictures/Badges-and-Labels-PNG/Gold_Badge_Transparent_PNG_Image.png"
+                ]
+            },
+            {
+                "label": "seeds_coefficient_x10000",
+                "value": [
+                    "int64",
+                    10500
+                ]
+			},
+			{
+                "label": "hypha_coefficient_x10000",
+                "value": [
+                    "int64",
+                    10500
+                ]
+			},
+			{
+                "label": "hvoice_coefficient_x10000",
+                "value": [
+                    "int64",
+                    10500
+                ]
+			},
+			{
+                "label": "husd_coefficient_x10000",
+                "value": [
+                    "int64",
+                    10500
+                ]
+            }
+        ]
+    ]
+}`
+
+const librarian_badge_assignment = `{
+    "content_groups": [
+        [
+            {
+                "label": "content_group_label",
+                "value": [
+                    "string",
+                    "details"
+                ]
+            },
+            {
+                "label": "title",
+                "value": [
+                    "string",
+                    "Librarian Badge Assignment"
+                ]
+            },
+            {
+                "label": "description",
+                "value": [
+                    "string",
+                    "Librarians are responsible for helping to construct new badges"
+                ]
+            },
+            {
+                "label": "start_period",
+                "value": [
+                    "int64",
+                    2
+                ]
+            },
+            {
+                "label": "end_period",
+                "value": [
+                    "int64",
+                    9
+                ]
+            }
+        ]
+    ]
+}`
+
+const single_token_coefficient = `{
+    "content_groups": [
+        [
+            {
+                "label": "content_group_label",
+                "value": [
+                    "string",
+                    "details"
+                ]
+            },
+            {
+                "label": "title",
+                "value": [
+                    "string",
+                    "Single Coefficient"
+                ]
+            },
+            {
+                "label": "description",
+                "value": [
+                    "string",
+                    "Ensure that other coefficients default to 10000 if they are not provided"
+                ]
+            },
+            {
+                "label": "icon",
+                "value": [
+                    "string",
+                    "https://gallery.yopriceville.com/var/resizes/Free-Clipart-Pictures/Badges-and-Labels-PNG/Gold_Badge_Transparent_PNG_Image.png"
+                ]
+            },
+            {
+                "label": "hypha_coefficient_x10000",
+                "value": [
+                    "int64",
+                    10230
+                ]
+			}
+        ]
+    ]
+}`
+
+const single_coeff_assignment = `{
+    "content_groups": [
+        [
+            {
+                "label": "content_group_label",
+                "value": [
+                    "string",
+                    "details"
+                ]
+            },
+            {
+                "label": "title",
+                "value": [
+                    "string",
+                    "Single Coefficient Assignment"
+                ]
+            },
+            {
+                "label": "description",
+                "value": [
+                    "string",
+                    "Only HYPHA token should have an increase on this badge"
+                ]
+            },
+            {
+                "label": "start_period",
+                "value": [
+                    "int64",
+                    2
+                ]
+            },
+            {
+                "label": "end_period",
+                "value": [
+                    "int64",
+                    9
+                ]
+            }
+        ]
+    ]
+}`
